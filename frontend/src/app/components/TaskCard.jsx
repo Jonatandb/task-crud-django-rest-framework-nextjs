3bcfde3c567b14e2fd362b8db87962fd2cb8388b
Jonatandb <jonatandb@gmail.com>
2024-05-06 11:05:51 -0300

    Add toggle task state functionality

--- frontend/src/app/components/TaskCard.jsx
+++ frontend/src/app/components/TaskCard.jsx
@@ -17,13 +17,33 @@ const TaskCard = ({ task }) => {
     }
   }
 
+  const handleTaskDone = async (id) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tasks/${id}/done/`, {
+      method: 'POST'
+    })
+    if (res.status == 200) {
+      router.refresh()
+    }
+  }
+
   return (
     <div className="bg-slate-500 px-4 py-3 mb-2 rounded-md text-slate-200 flex justify-between items-center">
       <div>
-        <h2 className="font-bold">{task.title}</h2>
+        <h2 className="font-bold">
+          {task.done && <span>✅</span>}&nbsp;{task.title}
+        </h2>
         <p>{task.description}</p>
       </div>
       <div className="flex justify-between gap-x-2">
+        <button
+          className={
+            "text-white rounded-md p-2" +
+            (task.done ? ' bg-gray-800' : ' bg-green-500')
+          }
+          onClick={() => handleTaskDone(task.id)}
+        >
+          {task.done ? 'Desmarcar' : 'Marcar'}
+        </button>
         <button
           className="bg-red-500 text-white rounded-md p-2"
           onClick={() => handleDelete(task.id)}
